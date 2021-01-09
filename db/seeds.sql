9ece7f2bff6af8b45161fd65b79c06ced7a834d9
calebkw <walke1291@gmail.com>
2021-01-09 09:44:08 -0600

    changed burger names

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", true);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Will you chive my car burger", false);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Not this thyme burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Girls Just Wanna Have Fennel Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("To Err is Cumin Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Summer Thyme Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Baby You Can Chive My Car Burger", false);
